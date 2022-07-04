13d6212110cd20085e3bfd06123d104b0a2b6b8a
Linh Vu <linh777gkog@gmail.com>
2022-07-04 22:57:10 +0700

    make draw action

--- script.js
+++ script.js
@@ -26,3 +26,17 @@ canvas.addEventListener('mousemove', (event) => {
 
 c.fillStyle = 'hotpink';
 c.strokeStyle = c.fillStyle;
+
+function draw(x2, y2) {
+  if (isDrawing) {
+    c.beginPath();
+    c.arc(x2, y2, penSize, 0, Math.PI * 2);
+    c.closePath();
+    c.fill();
+    //draw line
+    drawLine(x, y, x2, y2);
+  }
+
+  x = x2;
+  y = y2;
+}
